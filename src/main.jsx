440569bcc4ca32ef89da44b49791e28f4f7aaa88
SM991-web <mukhopadhyaysnehanshu@gmail.com>
2025-06-25 17:51:51 +0530

    commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { AuthProvider } from './contexts/AuthContext';
 import { HashRouter } from 'react-router-dom';
+import { AuthProvider } from './contexts/AuthContext';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-   <HashRouter basename="/travel-agency">
+  <HashRouter >
     <AuthProvider>
       <App />
     </AuthProvider>
